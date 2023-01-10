bd55b801318fcd36d18efb73d6aa0e62ce5a39be
Ezequiel Ferraris <Eze@Air-de-Ezequiel.home>
2023-01-10 16:02:53 -0300

    Primeros 5 ejercicios de la parte 1

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,9 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
+{	
+	let value = prompt("Ingrese un nombre: ");
+	txtIdNombre.value = value;
 	alert("ok");
 }
 
