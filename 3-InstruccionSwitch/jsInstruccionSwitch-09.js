bc23155838e2b92b0c0468cb0b9518ab041c5db3
Ezequiel Ferraris <Eze@Air-de-Ezequiel.home>
2023-01-13 13:23:56 -0300

    Ejercicios 9 y 10 de la tercera parte completos

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,64 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destino = document.getElementById("txtIdDestino").value;
+	let estacion = document.getElementById("txtIdEstacion").value;
+	let valorBase = 15000;
+	let precio;
+
+	switch(estacion) {
+		case "Invierno":
+			switch(destino) {
+				case "Cataratas":
+				case "Cordoba":
+					precio = valorBase - valorBase * 0.10;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+				case "Mar del plata":
+					precio = valorBase - valorBase * 0.20;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+				case "Bariloche":
+					precio = valorBase + valorBase * 0.2;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+		
+		};
+		break;
+
+		case "Verano":
+			switch(destino) {
+				case "Cataratas":
+				case "Cordoba":
+					precio = valorBase + valorBase * 0.10;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+				case "Mar del plata":
+					precio = valorBase + valorBase * 0.20;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+				case "Bariloche":
+					precio = valorBase - valorBase * 0.20;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+			};
+			break;
+
+		case "Primavera":
+		case "Otoño":
+			switch(destino) {
+				case "Cataratas":
+				case "Mar del plata":
+				case "Bariloche":
+					precio = valorBase + valorBase * 0.10;
+					alert(`La estadía cuesta $${precio}`);
+					break;
+				case "Cordoba":
+					alert(`La estadía cuesta $${valorBase}`);
+					break;
+			};
+		break;
+	};
+
+	
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,60 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destino = document.getElementById("txtIdDestino").value;
+	let estacion = document.getElementById("txtIdEstacion").value;
+	let mensaje1 = "No se viaja";
+	let mensaje2= "Se viaja";
+
+	switch(estacion) {
+		case "Invierno":
+			switch(destino) {
+				case "Cataratas":
+				case "Cordoba":
+				case "Mar del plata":
+					alert(`${mensaje1}`);
+					break;
+				case "Bariloche":
+					alert(`${mensaje2}`);
+					break;
+		};
+			break;
+
+		case "Verano":
+			switch(destino) {
+				case "Cordoba":
+				case "Bariloche":
+					alert(`${mensaje1}`);
+					break;
+				case "Cataratas":
+				case "Mar del plata":
+					alert(`${mensaje2}`);
+					break;
+			};
+			break;
+
+		case "Primavera":
+			switch(destino) {
+				case "Bariloche":
+					alert(`${mensaje1}`);
+					break;
+				case "Cataratas":
+				case "Cordoba":
+				case "Mar del plata":
+					alert(`${mensaje2}`);
+					break;
+			};
+			break;
+
+		case "Otoño":
+			switch(destino) {
+				case "Cataratas":
+				case "Cordoba":
+				case "Mar del plata":
+				case "Bariloche":
+					alert(`${mensaje2}`);
+					break;
+			};
+			break;
+	};
 
 }//FIN DE LA FUNCIÓN
